a89208fe3d8396a38d62020007738096fe54e28d
leakassandra <lea.krumbach@student.uni-tuebingen.de>
2025-02-25 15:35:50 +0100

    gear game done and updates chap2

--- Assets/Scripts/minigame3/GearManager.cs
+++ Assets/Scripts/minigame3/GearManager.cs
@@ -1,19 +1,21 @@
 using UnityEngine;
 using Fungus;
+using System.Collections;
 
 public class GearManager : MonoBehaviour
 {
     public static GearManager Instance;
 
-    private int totalGears;
+    public GameObject[] staticGears;
 
-    [SerializeField] private Flowchart flowchartChap1; // Assign Flowchart_Chap_1 in Inspector
+    private int totalGears;
+    [SerializeField] private Flowchart flowchartChap1; // Assign Flowchart in Inspector
     public GearArea[] areas;
 
     void Awake()
     {
         Instance = this;
-        totalGears = FindObjectsOfType<Gear>().Length; 
+        totalGears = FindObjectsOfType<Gear>().Length;
     }
 
     public void OnGearPlaced()
@@ -22,27 +24,25 @@ public class GearManager : MonoBehaviour
 
         bool allCorrect = true;
 
-        // Check every area to see if it has the correct gear
         foreach (var area in areas)
         {
-            if (!area.hasCorrectGear) {
+            if (!area.hasCorrectGear)
+            {
                 allCorrect = false;
                 break;
             }
         }
 
-        // Only call the success block if all gears are correct
         if (allCorrect)
         {
-            Debug.Log("All correct gears placed! Executing 'Gear_solved'.");
-            CallGameSuccessBlock();
+            Debug.Log("All correct gears placed! Executing 'Gear_solved' and starting rotation.");
+            StartCoroutine(RotateGearsForSeconds(5f)); // 5 second animation
         }
         else
         {
             Debug.Log("Not all gears are placed correctly yet.");
         }
     }
-
 
     private void CallGameSuccessBlock()
     {
@@ -55,4 +55,35 @@ public class GearManager : MonoBehaviour
             Debug.LogError("Gear_solved is not assigned in the Inspector!");
         }
     }
+
+    private IEnumerator RotateGearsForSeconds(float duration)
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            foreach (var area in areas)
+            {
+                if (area.currentGear != null)
+                {
+                    area.currentGear.transform.Rotate(0, 0, 100 * Time.deltaTime);
+                }
+            }
+
+            foreach (var gear in staticGears)
+            {
+                if (gear != null)
+                {
+                    gear.transform.Rotate(0, 0, 100 * Time.deltaTime);
+                }
+            }
+
+            elapsedTime += Time.deltaTime;
+            yield return null; 
+        }
+
+        Debug.Log("Gear rotation finished! Now executing Gear_solved.");
+        CallGameSuccessBlock();
+    }
 }
+
